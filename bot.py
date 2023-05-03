61af37b364d6097f39cc32c4c43c4224b5e80b39
Alexander Pervyshev <kingtiger27062004@gmail.com>
2023-05-03 21:54:33 +0300

    for my first tg.bot

--- bot.py
+++ bot.py
@@ -1,12 +1,32 @@
 # bot.py
 import os
+import logging
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext.filters import BaseFilter
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info(
+        f"Message from chat #{update.effective_chat.id}"
+        f"Message {update.effective_user.first_name}"
+    )
+    await update.message.reply_text(
+        f"Hello {update.effective_user.first_name} ({update.effective_chat.id})"
+    )
+
+
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info(
+        f"Message from chat #{update.effective_chat.id}"
+        f"Message {update.effective_user.first_name}"
+    )
     await update.message.reply_text(
         f"Hello {update.effective_user.first_name} ({update.effective_chat.id})"
     )
@@ -14,5 +34,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(~filters.COMMAND, message))
     app.run_polling()
-
